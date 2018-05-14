09542b6ae21d5867d31b0579256d33ea15334202
Shannon Foster <ShannonCanTech@users.noreply.github.com>
2018-05-14 18:07:06 -0400

    Correction made
    Wrong code pushed. Correction made.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,30 +6,17 @@ public class Main {
 
     public static void main(String[] args) {
         //Enter up to 5 numbers
-        boolean done = false;
-        int[] inputArray = {1, 4, -6, 5, 8};
-/*        int product = inputArray[0];
-        for (int i = 0; i < inputArray.length; i++) {
+        int[] inputArray = new int[5];
+        Scanner scan = new Scanner(System.in);
 
-            if (inputArray[i] > product ){
-                product = inputArray[i];
-            }
-        }
-        adjacentElementsProduct(inputArray);*/
-
-        System.out.println(adjacentElementsProduct(inputArray) + " is the highest number.");
-
-/*        Scanner scan = new Scanner(System.in);
-
-        do {
-            for (int x = 0; x < 5; x++) {
-                System.out.println("Create an array: ");
-                input = scan.nextInt();
-                input.add(inputArray[input]);
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Insert number: ");
+                inputArray[i] = scan.nextInt();
                 scan.nextLine();
             }
-        } while (!done);
-        adjacentElementsProduct(inputArray);*/
+
+
+        System.out.println(adjacentElementsProduct(inputArray) + " is the highest number.");
     }
 
 
@@ -45,15 +32,5 @@ public class Main {
             }
 
             return product;
-
-/*            //product means multiplying
-            int x =  inputArray.length(); //the array is x long
-            //which of these numbers in the array produces the greatest number
-
-            //prints each number in the array while looping
-            for (int i = 0; i <= inputArray.length(); i++) {
-                inputArray[i];
-            }
-            return i * x;*/
         }
 }
